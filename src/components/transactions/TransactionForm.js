5eaa0158940c94fdefe7ce882023aedece56de72
Sushanth08k <ksushanth9030@gmail.com>
2026-01-05 11:00:00 +0530

    Sanitize and trim TransactionForm payload before submit

--- src/components/transactions/TransactionForm.js
+++ src/components/transactions/TransactionForm.js
@@ -78,6 +78,9 @@ function TransactionForm({ onTransactionAdded }) {
     }
 
     fetchDistricts();
+    return () => {
+      isMounted = false;
+    };
   }, [formData.stateId]);
 
   const handleChange = (e) => {
